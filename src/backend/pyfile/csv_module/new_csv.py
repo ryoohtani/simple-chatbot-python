608266cd330b533a811cc9715c67397844da973d
ryoohtani <kazuinto64@gmail.com>
2023-07-30 21:32:24 +0900

    csvファイルの生成機能
    Pythonで実装中

---
+++ src/backend/pyfile/csv_module/new_csv.py
@@ -0,0 +1,6 @@
+# from csv_module import main
+
+def make_csv_file(input_file):
+    input_file = "./file_saving/input.csv"
+    with open(input_file, "w") as file:
+        file.write("")
---
+++ src/backend/pyfile/main.py
@@ -0,0 +1,12 @@
+from csv_module.new_csv import make_csv_file
+import os
+
+def csv_file(input_file):
+    if input_file != "":
+        print("ファイル名:", input_file)
+        make_csv_file(input_file)
+    else:
+        print("入力してください")
+
+input_file = input("csvファイルへの記載を行ってください \n")
+csv_file(input_file)
