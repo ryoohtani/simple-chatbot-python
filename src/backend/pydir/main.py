02dfce832d64086cfc2af0c16bcbd3e0e91b5dfe
ryoohtani <kazuinto64@gmail.com>
2023-08-05 15:44:34 +0900

    chatbotメイン処理について
    main関数からaルートいく処理を追加

--- src/backend/pydir/main.py
+++ src/backend/pydir/main.py
@@ -1,17 +1,18 @@
 import questionary
+from sub_module.section_1 import a_end
 
 def main_bot():
     print("おすすめ検索エンジン紹介！！")
 
     question_one = questionary.select(
         "好きなの方は？",
-        choices = ["やっぱ最新でしょ！", "やっぱ古いのでしょ！", "該当なし"],
+        choices = ["やっぱ王道でしょ！", "やっぱ人と違うのでしょ！", "特にないかな〜"],
         use_arrow_keys = True
     ).ask()
 
-    if question_one == "やっぱ最新でしょ！":
-        print("テスト1")
-    elif question_one == "やっぱ古いのでしょ！":
+    if question_one == "やっぱ王道でしょ！":
+        a_end()
+    elif question_one == "やっぱ人と違うのでしょ！":
         print("テスト2")
     else:
         print("エラー")
