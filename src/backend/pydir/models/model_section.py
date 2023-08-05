7044deb63221524af10925e86e922ab7e2bf39f8
ryoohtani <kazuinto64@gmail.com>
2023-08-05 21:48:45 +0900

    選択機能の修正(MVCモデルの実装)
    選択機能の一部をモデルに作成

---
+++ src/backend/pydir/models/model_section.py
@@ -0,0 +1,38 @@
+import questionary
+
+class SectionModels(object):
+    def all_question(self, question_one):
+        print("最後の設問")
+
+        if question_one == "やっぱ王道でしょ！":
+            select_routo = questionary.select(
+                "スマホに入れているアプリは？",
+                choices = ["Chrome", "Safari"],
+                use_arrow_keys = True
+            ).ask()
+            if select_routo == "Chrome":
+                print("あなたにオススメなサイトはChromeです!\n", "https://www.google.co.jp/")
+            else:
+                print("あなたにオススメなサイトはSafariです!\n", "https://www.apple.com/safari/")
+
+        elif question_one == "やっぱ人と違うのでしょ！":
+            select_routo = questionary.select(
+                "PCに入れているアプリは？",
+                choices = ["FireFox", "Bing"],
+                use_arrow_keys = True
+            ).ask()
+            if select_routo == "FireFox":
+                print("あなたにオススメなサイトはFireFoxです!\n", "https://www.mozilla.org/firefox/")
+            else:
+                print("あなたにオススメなサイトはBingです!\n", "https://www.bing.com/")
+
+        else:
+            select_routo = questionary.select(
+                "検索エンジンぐらいは冒険してみませんか？",
+                choices = ["YANDEX", "DuckDuckGo"],
+                use_arrow_keys = True
+            ).ask()
+            if select_routo == "YANDEX":
+                print("あなたにオススメなサイトはYANDEXです!\n", "https://yandex.com/")
+            else:
+                print("あなたにオススメなサイトはDuckDuckGoです!\n", "https://duckduckgo.com/")
